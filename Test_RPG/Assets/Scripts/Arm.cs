3670859b1e1532e8ce233bfd4056f6bf2bb646d2
aLIEz2476 <sakuragun11@gmail.com>
2018-04-24 17:49:02 +0900

    1
    1

---
+++ Test_RPG/Assets/Scripts/Arm.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : MonoBehaviour {
+    public float m_fAttackSpeed; public float m_fRotSpeed;
+    bool m_bAttack = false, m_bRelease = true;
+    float m_fCurRot = 0, m_fMaxRot = 45;
+    public GameObject m_objArm;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        if (m_bRelease)
+            ReleaseArmUpdate();
+
+	}
+
+
+    protected void MoveArm(float rotspeed)
+    {
+        float fRotSpeed = rotspeed * Time.deltaTime;
+        m_objArm.transform.Rotate(Vector3.right * fRotSpeed);
+    }
+
+
+    protected IEnumerator AttackArm()
+    {
+        float fMaxRot = 45, fCurRot = 0;
+        while (fCurRot < fMaxRot)
+        {
+            MoveArm(m_fRotSpeed);
+            fCurRot += m_fRotSpeed;
+            yield return null;
+        }
+        StartCoroutine("ReleaseArm");
+
+    }
+
+    protected IEnumerator ReleaseArm()
+    {
+        float fMaxRot = 45, fCurRot = 0;
+        while (fCurRot < fMaxRot)
+        {
+            MoveArm(-m_fRotSpeed);
+            fCurRot += m_fRotSpeed;
+            yield return null;
+        }
+        m_bAttack = false;
+
+    }
+
+    // 코루틴을 사용하지 않을 경우, 맴버변수를 만들어 매프레임마다 호출확인을 해야한다.
+    protected void AttackArmUpdate()
+    {
+        if (m_fCurRot < m_fMaxRot)
+        {
+            MoveArm(m_fRotSpeed);
+            m_fCurRot += m_fRotSpeed;
+
+        }
+        else
+        {
+            m_bAttack = false;
+            m_fCurRot = 0;
+        }
+    }
+
+    protected void ReleaseArmUpdate()
+    {
+        if (m_fCurRot < m_fMaxRot)
+        {
+            MoveArm(-m_fRotSpeed);
+            m_fCurRot += m_fRotSpeed;
+
+        }
+        else
+        {
+            m_bAttack = false;
+            m_fCurRot = 0;
+            m_bRelease = false;
+        }
+    }
+
+    public void Init(float Attack)
+    {
+        m_fAttackSpeed = Attack;
+    }
+}
---
+++ Test_RPG/Assets/Scripts/Dynamic.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dynamic : MonoBehaviour
+{
+    int m_nKeyCount; public float m_fMoveSpeed; public float m_fRotSpeed;
+    public GameObject m_objArm; public Arm m_cArm; bool m_bAttack;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    Debug.Log("Space hit : " + m_nKeyCount);
+        //    m_nKeyCount++;
+        //}
+        
+        InputProcess();
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 100, 20), string.Format("{0}", m_nKeyCount));
+        // Rect(x, y, 넓이, 높이)
+
+    }
+    // 테스트용 UI셔틀
+
+   
+    // 코루틴을 사용하면, 매 프레임마다 반복문의 한 단계가 실행되므로, 
+    // 따로 끝날 때 처리할 필요가 없으며, 해당반복의 단계가 끝나면 실행이 끝난다
+    // 실행중 지역변수들은 코루틴이 끝날 때까지 유지된다.
+    
+
+    void InputProcess()
+    {
+        float fMoveDist = m_fMoveSpeed * Time.deltaTime;
+        float fRotAngle = m_fRotSpeed * Time.deltaTime;
+
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * fMoveDist);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * fMoveDist);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * -fRotAngle);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * fRotAngle);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 일반적으로 코루틴을 이용할 때에는 코루틴을 부를 때마다
+            // 해당 반복문이 실행되므로, 중복해서 계속 동작한다.
+            
+
+            if(m_bAttack==false)
+            {
+                StartCoroutine("AttackArm");
+                m_bAttack = true;
+            }
+
+            m_cArm.Init(50);
+
+        }
+    }
+}
